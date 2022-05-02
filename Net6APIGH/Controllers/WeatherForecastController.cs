a8a9780202db98987738ab4b2cad08eb0d5820da
Shashi Jeevan M. P <shashijeevan@tekfriday.com>
2022-05-02 12:21:22 +0530

    Alignment correction

--- Net6APIGH/Controllers/WeatherForecastController.cs
+++ Net6APIGH/Controllers/WeatherForecastController.cs
@@ -4,12 +4,12 @@ namespace Net6APIGH.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
-    {
-        private static readonly string[] Summaries = new[]
+        public class WeatherForecastController : ControllerBase
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+            private static readonly string[] Summaries = new[]
+            {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
